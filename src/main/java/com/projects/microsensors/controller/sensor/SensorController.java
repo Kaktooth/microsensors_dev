80e018bb682e806a9a40b3a2f872630c4732435b
Volodimir Bezridniy <48326107+Kaktooth@users.noreply.github.com>
2022-06-04 20:45:06 +0300

    remove records

--- src/main/java/com/projects/microsensors/controller/sensor/SensorController.java
+++ src/main/java/com/projects/microsensors/controller/sensor/SensorController.java
@@ -3,6 +3,7 @@ package com.projects.microsensors.controller.sensor;
 import com.projects.microsensors.model.SensorRequest;
 import com.projects.microsensors.service.SensorService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("api/v1/sensors")
-public record SensorController(SensorService sensorService) {
+public class SensorController {
+
+    private final SensorService sensorService;
+
+    @Autowired
+    public SensorController(SensorService sensorService) {
+        this.sensorService = sensorService;
+    }
 
     @PostMapping
     public void saveSensor(@RequestBody SensorRequest sensorRequest) {
