81afe413a20f0b34600a67c354eb15f5e1df7735
Volodimir Bezridniy <48326107+Kaktooth@users.noreply.github.com>
2022-06-05 10:39:26 +0300

    Fix bug. Remove request body in parameters.

--- src/main/java/com/projects/microsensors/controller/sensor/SensorController.java
+++ src/main/java/com/projects/microsensors/controller/sensor/SensorController.java
@@ -24,7 +24,7 @@ public class SensorController {
     }
 
     @GetMapping
-    public String sensorPage(@RequestBody SensorRequest sensorRequest) {
+    public String sensorPage() {
         return "upload sensors... ";
     }
 
