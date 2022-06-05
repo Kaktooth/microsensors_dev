f72a9a6f7e292090b2e6c04f680d668ae08f931e
Volodimir Bezridniy <48326107+Kaktooth@users.noreply.github.com>
2022-06-05 10:46:52 +0300

    change classes to records.

--- src/main/java/com/projects/microsensors/model/SensorDataRequest.java
+++ src/main/java/com/projects/microsensors/model/SensorDataRequest.java
@@ -1,22 +1,10 @@
 package com.projects.microsensors.model;
 
-import lombok.Getter;
-
 import java.util.UUID;
 
-@Getter
-public class SensorDataRequest {
-    private final UUID id;
-    private final byte[] data;
-    private final UUID sensorId;
-
-    public SensorDataRequest(
-        UUID id,
-        byte[] data,
-        UUID sensorId
-    ) {
-        this.id = id;
-        this.data = data;
-        this.sensorId = sensorId;
-    }
+public record SensorDataRequest(
+    UUID id,
+    byte[] data,
+    UUID sensorId
+) {
 }
--- src/main/java/com/projects/microsensors/model/SensorRequest.java
+++ src/main/java/com/projects/microsensors/model/SensorRequest.java
@@ -1,21 +1,9 @@
 package com.projects.microsensors.model;
 
-import lombok.Getter;
-
 import java.util.UUID;
 
-@Getter
-public class SensorRequest {
-    private final UUID id;
-    private final String name;
-    private final String sensorInfo;
-
-    public SensorRequest(
-        UUID id,
-        String name,
-        String sensorInfo) {
-        this.id = id;
-        this.name = name;
-        this.sensorInfo = sensorInfo;
-    }
+public record SensorRequest(
+    UUID id,
+    String name,
+    String sensorInfo) {
 }
