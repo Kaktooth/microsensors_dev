74c251c925494d09ee8a65a32fe85e86a63e1a26
Volodimir Bezridniy <48326107+Kaktooth@users.noreply.github.com>
2022-06-05 20:21:40 +0300

    remove date from request

--- src/main/java/com/projects/microsensors/model/SensorMessageRequest.java
+++ src/main/java/com/projects/microsensors/model/SensorMessageRequest.java
@@ -1,7 +1,6 @@
 package com.projects.microsensors.model;
 
-import java.sql.Timestamp;
 import java.util.UUID;
 
-public record SensorMessageRequest(UUID id, Timestamp receiveDate, String message, UUID sensorId) {
+public record SensorMessageRequest(UUID id, String message, UUID sensorId) {
 }
