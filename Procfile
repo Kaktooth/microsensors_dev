0fc1e8bff5fb59bebdaac966c9081c7a76d0abab
Volodimir Bezridniy <48326107+Kaktooth@users.noreply.github.com>
2022-06-05 10:32:12 +0300

    Reset to records and edit heroku files and gradle file.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -Dserver.port=$PORT -jar sensor/build/libs/sensor-0.0.1-SNAPSHOT.jar
+web: java -Dserver.port=$PORT -jar build/libs/microsensors-0.0.1-SNAPSHOT.jar
--- src/main/java/com/projects/microsensors/controller/sensor/SensorController.java
+++ src/main/java/com/projects/microsensors/controller/sensor/SensorController.java
@@ -3,6 +3,7 @@ package com.projects.microsensors.controller.sensor;
 import com.projects.microsensors.model.SensorRequest;
 import com.projects.microsensors.service.SensorService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/sensors")
 public record SensorController(SensorService sensorService) {
+
+    @GetMapping
+    public String sensorPage(@RequestBody SensorRequest sensorRequest) {
+        return "upload sensors... ";
+    }
 
     @PostMapping
     public void saveSensor(@RequestBody SensorRequest sensorRequest) {
