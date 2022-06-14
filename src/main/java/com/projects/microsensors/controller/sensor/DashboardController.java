0515c2f228c771c20b1886341d89ff9166919a3c
Volodimir Bezridniy <48326107+Kaktooth@users.noreply.github.com>
2022-06-14 18:45:05 +0300

    Edit param.

--- src/main/java/com/projects/microsensors/controller/sensor/DashboardController.java
+++ src/main/java/com/projects/microsensors/controller/sensor/DashboardController.java
@@ -6,8 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -22,7 +22,7 @@ public record DashboardController(SensorService sensorService) {
     }
 
     @PostMapping
-    public String createNewSensor(@RequestBody SensorRequest sensorRequest) {
+    public String createNewSensor(@ModelAttribute SensorRequest sensorRequest) {
         log.info("new sensor {}", sensorRequest);
         sensorService.saveSensor(sensorRequest);
         return "dashboard";
