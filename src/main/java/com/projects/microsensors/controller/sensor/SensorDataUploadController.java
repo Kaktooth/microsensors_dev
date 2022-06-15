15320fe5fbf25c84721698a46cb6910c946c6376
Volodimir Bezridniy <48326107+Kaktooth@users.noreply.github.com>
2022-06-15 11:36:11 +0300

    Edit data controller.

--- src/main/java/com/projects/microsensors/controller/sensor/SensorDataUploadController.java
+++ src/main/java/com/projects/microsensors/controller/sensor/SensorDataUploadController.java
@@ -4,19 +4,17 @@ import com.projects.microsensors.model.SensorDataRequest;
 import com.projects.microsensors.service.SensorDataService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.rest.core.event.AfterCreateEvent;
-import org.springframework.data.rest.core.event.BeforeCreateEvent;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController
 @AllArgsConstructor
+@RepositoryRestController
 @RequestMapping("api/v1/sensor-data")
 public class SensorDataUploadController {
 
--- src/main/java/com/projects/microsensors/model/SensorMessageEventHandler.java
+++ src/main/java/com/projects/microsensors/model/SensorMessageEventHandler.java
@@ -2,7 +2,6 @@ package com.projects.microsensors.model;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.rest.core.annotation.HandleAfterCreate;
-import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.stereotype.Controller;
 
