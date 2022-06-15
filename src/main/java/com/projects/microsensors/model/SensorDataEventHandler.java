a390fb9ef8b9a79515c1faadfac4ba4bd4add85a
Volodimir Bezridniy <48326107+Kaktooth@users.noreply.github.com>
2022-06-15 15:10:22 +0300

    Extend AbstractRepositoryEventListener

--- src/main/java/com/projects/microsensors/model/SensorDataEventHandler.java
+++ src/main/java/com/projects/microsensors/model/SensorDataEventHandler.java
@@ -3,12 +3,13 @@ package com.projects.microsensors.model;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.rest.core.annotation.HandleAfterCreate;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.data.rest.core.event.AbstractRepositoryEventListener;
 import org.springframework.stereotype.Controller;
 
 @Slf4j
 @Controller
 @RepositoryEventHandler(SensorData.class)
-public class SensorDataEventHandler {
+public class SensorDataEventHandler extends AbstractRepositoryEventListener<SensorData> {
 
     @HandleAfterCreate
     public String handleAuthorAfterCreate(SensorData data) {
--- src/main/java/com/projects/microsensors/model/SensorMessageEventHandler.java
+++ src/main/java/com/projects/microsensors/model/SensorMessageEventHandler.java
@@ -3,12 +3,13 @@ package com.projects.microsensors.model;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.rest.core.annotation.HandleAfterCreate;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.data.rest.core.event.AbstractRepositoryEventListener;
 import org.springframework.stereotype.Controller;
 
 @Slf4j
 @Controller
 @RepositoryEventHandler(SensorMessage.class)
-public class SensorMessageEventHandler {
+public class SensorMessageEventHandler extends AbstractRepositoryEventListener<SensorMessage> {
 
     @HandleAfterCreate
     public String handleAuthorAfterCreate(SensorMessage message) {
