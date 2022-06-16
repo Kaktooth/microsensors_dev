8e39a9aa5fbccd750ad717990219ba51d7e68e60
Volodimir Bezridniy <48326107+Kaktooth@users.noreply.github.com>
2022-06-16 17:02:20 +0300

    -fix attribute

--- src/main/java/com/projects/microsensors/controller/sensor/DashboardController.java
+++ src/main/java/com/projects/microsensors/controller/sensor/DashboardController.java
@@ -7,12 +7,12 @@ import com.projects.microsensors.service.sensor.SensorService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.UUID;
@@ -26,20 +26,17 @@ public class DashboardController {
     private final SensorDTOService sensorDTOService;
 
     @GetMapping
-    public String getDashboard(RedirectAttributes redirectAttributes,
-                               @RequestParam(value = "sensorId", required = false)
-                                   String sensorId) {
+    public String getDashboard(Model model, @RequestParam(value = "sensorId", required = false) String sensorId) {
         if (sensorId != null) {
-            redirectAttributes.addAttribute("sensor",
+            model.addAttribute("sensor",
                 sensorDTOService.getSensorDTO(UUID.fromString(sensorId)));
         }
         log.info("loading dashboard");
-        redirectAttributes.addFlashAttribute("sensorId", sensorId);
+        model.addAttribute("sensorId", sensorId);
         List<Sensor> sensorList = sensorService.getAllSensors();
-        redirectAttributes.addAttribute("sensorList", sensorList);
-
-
-        redirectAttributes.addAttribute("sensorRequest", new SensorRequest());
+        model.addAttribute("sensorList", sensorList);
+        sensorList.add(new Sensor(UUID.randomUUID(),"new sensor","info"));
+        model.addAttribute("sensorRequest", new SensorRequest());
 
         return "dashboard";
     }
