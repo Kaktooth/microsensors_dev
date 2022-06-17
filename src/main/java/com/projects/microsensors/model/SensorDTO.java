2bf9ec0f906fcede2c1dcad3d4ff34a788734a50
Volodimir Bezridniy <48326107+Kaktooth@users.noreply.github.com>
2022-06-17 11:38:00 +0300

    -make dto class transactional

--- src/main/java/com/projects/microsensors/model/SensorDTO.java
+++ src/main/java/com/projects/microsensors/model/SensorDTO.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.Value;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Transactional
 public class SensorDTO {
     UUID id;
     String name;
