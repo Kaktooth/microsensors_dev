6129e45070b9f4976d4e82dd495c69255851f641
Volodimir Bezridniy <48326107+Kaktooth@users.noreply.github.com>
2022-06-17 11:08:13 +0300

    -fix lazy init error

--- src/main/java/com/projects/microsensors/model/SensorDTO.java
+++ src/main/java/com/projects/microsensors/model/SensorDTO.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.Value;
+import org.hibernate.annotations.Proxy;
 
 import java.util.List;
 import java.util.UUID;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Proxy(lazy=false)
 public class SensorDTO {
     UUID id;
     String name;
