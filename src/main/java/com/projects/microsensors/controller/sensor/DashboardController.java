88dd003a604b3eba770ef1c79572207ebeaf23a9
Volodimir Bezridniy <48326107+Kaktooth@users.noreply.github.com>
2022-06-18 12:43:33 +0300

    -set textarea scroll bar to bottom

--- src/main/java/com/projects/microsensors/controller/sensor/DashboardController.java
+++ src/main/java/com/projects/microsensors/controller/sensor/DashboardController.java
@@ -15,10 +15,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 
 @Slf4j
