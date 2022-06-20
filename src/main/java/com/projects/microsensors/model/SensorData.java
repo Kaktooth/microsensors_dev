86760890b38742f69fa31e8a8ef8a043917c9908
Volodimir Bezridniy <48326107+Kaktooth@users.noreply.github.com>
2022-06-20 17:14:49 +0300

    - fix the data display

--- src/main/java/com/projects/microsensors/model/SensorData.java
+++ src/main/java/com/projects/microsensors/model/SensorData.java
@@ -11,6 +11,7 @@ import org.hibernate.Hibernate;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.sql.Timestamp;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.Objects;
 import java.util.UUID;
@@ -50,7 +51,7 @@ public class SensorData extends Domain implements Comparable<SensorData> {
 
     @Override
     public String toString() {
-        return " \uD83C\uDD83\uD83C\uDD78\uD83C\uDD7C\uD83C\uDD74: " + receiveDate.toLocalDateTime() + " \n" + new String(data) + "\n";
+        return " \uD83C\uDD83\uD83C\uDD78\uD83C\uDD7C\uD83C\uDD74: " + receiveDate.toLocalDateTime() + " \n" + Arrays.toString(Base64.getDecoder().decode(data)) + "\n";
     }
 
     @Override
