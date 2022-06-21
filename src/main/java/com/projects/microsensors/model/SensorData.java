49720a47b4d60662344f2cc19d957970295f6131
Volodimir Bezridniy <48326107+Kaktooth@users.noreply.github.com>
2022-06-21 14:58:59 +0300

    - remove char encoding

--- src/main/java/com/projects/microsensors/model/SensorData.java
+++ src/main/java/com/projects/microsensors/model/SensorData.java
@@ -52,10 +52,10 @@ public class SensorData extends Domain implements Comparable<SensorData> {
     @Override
     public String toString() {
         System.out.println(data);
-        return " \uD83C\uDD83\uD83C\uDD78\uD83C\uDD7C\uD83C\uDD74: "
+        return "\uD83C\uDD83\uD83C\uDD78\uD83C\uDD7C\uD83C\uDD74: "
             + receiveDate.toLocalDateTime()
-            + " \n Data: "
-            + new String(data, StandardCharsets.UTF_8)
+            + "\nData: "
+            + new String(data)
             + "\nBytes:" + Arrays.toString(data) + "\n";
     }
 
